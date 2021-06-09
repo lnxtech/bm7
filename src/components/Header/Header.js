42eeeeb1f4c5ccdea4d2cce5bccde595f3763df3
Maided Ponce <maided.h.p90@gmail.com>
2021-06-08 23:26:09 -0500

    añadiendo menu a traves de click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,23 @@
-import React from "react";
-import "../../assets/styles/Header.css";
+import React from 'react'
+import Menu from './Menu'
+import '../../assets/styles/Header.css'
 
 // assets
-import MenuIcon from "../../assets/static/menu-icon.png";
-import Bm7Icon from "../../assets/static/bm7-icon.png";
-import ProfileIcon from "../../assets/static/profile-icon.png";
+import Bm7Icon from '../../assets/static/bm7-icon.png'
+import ProfileIcon from '../../assets/static/profile-icon.png'
 
 const Header = () => {
   return (
-    <header className="nav__container">
-      <figure className="nav__img-container">
-        <img className="img_nav-1" src={MenuIcon} alt="menu" />
+    <header className='nav__container'>
+      <Menu />
+      <figure className='nav__img-container'>
+        <img className='img_nav-2' src={Bm7Icon} alt='bm7-icon' />
       </figure>
-      <figure className="nav__img-container">
-        <img className="img_nav-2" src={Bm7Icon} alt="bm7-icon" />
-      </figure>
-      <figure className="nav__img-container">
-        <img className="img_nav-3" src={ProfileIcon} alt="profile" />
+      <figure className='nav__img-container'>
+        <img className='img_nav-3' src={ProfileIcon} alt='profile' />
       </figure>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
